5de8cd814e1631c85f451c1ba7e3b08c4e62dd86
cflo <chrishops@users.noreply.github.com>
2021-02-08 00:42:53 -0600

    Update dl.js

--- commands/dl.js
+++ commands/dl.js
@@ -1,7 +1,8 @@
-const { MessageAttachment } = require("discord.js");
+const Discord = require("discord.js");
 const helper = require("../helper.js");
 const ytdl = require("youtube-dl");
 const fs = require("fs");
+const client = new Discord.Client();
 
 module.exports = {
   name: "dl",
@@ -15,27 +16,37 @@ module.exports = {
     const url = args[0].replace(/[<>]/g, "");
     const path = `./temp/video.mp4`;
 
-    if (args.length > 1) options = helper.JoinArgs(args.slice(1)).split(", ");
-    else options = ["--dump-json", "-f best"];
+    options = args.length > 1 ? helper.JoinArgs(args.slice(1)).split(", ") : ["--dump-json", "-f best"];
 
-    message.channel.send("Attempting to download file...").then(console.log(`${message.author.username} is downloading a video from ${url}`.yellow));
+    message.channel.send("Getting video information...").then(console.log(`${message.author.username} is attempting to download a video from ${url}`.yellow));
 
     const video = ytdl(url, options)
+      // check for error before we download file
+      .on("error", function error(error) {
+        message.reply(error.stderr);
+        console.log(error);
+      })
+      .on("info", function (info) {
+        message.channel.send("Downloading file...");
+      })
       .pipe(fs.createWriteStream(path))
+
+      // wait to send after it finishes downloading
       .on("close", function () {
-        let sendAttachment = new MessageAttachment(path);
+        let sendAttachment = new Discord.MessageAttachment(path);
         message.channel
           .send(sendAttachment)
           .then(message.channel.send("Uploading file..."))
+          // delete file after it finishes sending
+          .then(() => {
+            fs.unlink(path, err => {
+              if (err) throw err;
+            });
+          })
           .catch(error => {
             console.error(error);
             message.reply(error.message);
           });
-        //setTimeout(() => {
-        //  fs.unlink(path, err => {
-        //    if (err) throw err;
-        //  });
-        //}, 16000);
       });
   },
 };
