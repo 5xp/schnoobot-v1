e41d5204e59f7731955c98bd9374dfc809d984d4
cflo <chrishops@users.noreply.github.com>
2021-02-07 18:18:32 -0600

    Update dl.js

--- commands/dl.js
+++ commands/dl.js
@@ -11,25 +11,27 @@ module.exports = {
     const url = args[0].replace(/[<>]/g, "");
     const path = `./temp/video.mp4`;
 
-    const video = ytdl(url)
+    if (args.length > 1) options = helper.JoinArgs(args.slice(1)).split(", ");
+    else options = ["--dump-json", "-f best"];
+
+    message.channel.send("Attempting to download file...").then(console.log(`${message.author.username} is downloading a video from ${url}`.yellow));
+
+    const video = ytdl(url, options)
       .pipe(fs.createWriteStream(path))
       .on("close", function () {
         let sendAttachment = new MessageAttachment(path);
-        message.channel.send(sendAttachment).catch(error => {
-          console.error(error);
-          message.reply(error.message);
-        });
-        fs.unlink(path, err => {
-          if (err) throw err;
-        });
+        message.channel
+          .send(sendAttachment)
+          .then(message.channel.send("Uploading file..."))
+          .catch(error => {
+            console.error(error);
+            message.reply(error.message);
+          });
+        //setTimeout(() => {
+        //  fs.unlink(path, err => {
+        //    if (err) throw err;
+        //  });
+        //}, 16000);
       });
-
-    function DeleteVideo(path) {
-      try {
-        fs.unlink(path);
-      } catch (err) {
-        console.error(err);
-      }
-    }
   },
 };
