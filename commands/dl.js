3e54ba9a7b13ce3b384e9d73abec5d9a87ec12d4
cflo <chrishops@users.noreply.github.com>
2021-02-11 00:26:43 -0600

    Update dl.js
    variable file extension, polish with awaits

--- commands/dl.js
+++ commands/dl.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const helper = require("../helper.js");
 const ytdl = require("youtube-dl");
 const fs = require("fs");
-const client = new Discord.Client();
 
 module.exports = {
   name: "dl",
@@ -14,40 +13,61 @@ module.exports = {
       return;
     }
 
+    var ext;
     const url = args[0].replace(/[<>]/g, "");
-    const path = `./temp/video.mp4`;
+    const dir = `./temp`;
+    var GetPath = () => {
+      return `${dir}/output.${ext}`;
+    };
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
 
     options = args.length > 1 ? helper.JoinArgs(args.slice(1)).split(", ") : ["--dump-json", "-f best"];
 
-    message.channel.send("Getting video information...").then(console.log(`${message.author.username} is attempting to download a video from ${url}`.yellow));
+    async function sendMessage() {
+      const msg = await message.channel.send("Getting file information...").then(console.log(`${message.author.username} is attempting to download a video from ${url}`.yellow));
 
-    const video = ytdl(url, options)
-      // check for error before we download file
-      .on("error", function error(error) {
-        message.reply(error.stderr);
-        console.log(error);
-      })
-      .on("info", function (info) {
-        message.channel.send("Downloading file...");
-      })
-      .pipe(fs.createWriteStream(path))
-
-      // wait to send after it finishes downloading
-      .on("close", function () {
-        let sendAttachment = new Discord.MessageAttachment(path);
-        message.channel
-          .send(sendAttachment)
-          .then(message.channel.send("Uploading file..."))
-          // delete file after it finishes sending
-          .then(() => {
-            fs.unlink(path, err => {
-              if (err) throw err;
-            });
+      ytdl.getInfo(url, options, function (err, info) {
+        if (err) throw err;
+        ext = info.ext;
+        const video = ytdl(url, options)
+          // check for error before we download file
+          .on("error", function error(error) {
+            message.reply(error.stderr);
+            console.log(error);
           })
-          .catch(error => {
-            console.error(error);
-            message.reply(error.message);
+          .on("info", function (info) {
+            msg.edit("Downloading file...");
+          })
+          .pipe(fs.createWriteStream(GetPath()))
+
+          // wait to send after it finishes downloading
+          .on("close", function () {
+            let sendAttachment = new Discord.MessageAttachment(GetPath());
+            message.channel
+              .send(sendAttachment)
+              .then(msg.edit("Uploading file..."))
+              // delete file after it finishes sending
+              .then(() => {
+                msg.delete();
+                fs.unlink(GetPath(), err => {
+                  if (err) throw err;
+                });
+              })
+              .catch(error => {
+                console.error(error);
+                message.reply(error.message);
+              });
           });
       });
+    }
+
+    sendMessage();
+
+    process.on("unhandledRejection", error => {
+      message.reply(error.stderr);
+    });
   },
 };
