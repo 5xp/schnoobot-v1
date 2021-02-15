93de94e3334bcbb95341999a84c4219580e20c0d
cflo <chrishops@users.noreply.github.com>
2021-02-14 18:11:24 -0600

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const helper = require("../helper.js");
+
+module.exports = {
+  name: "avatar",
+  description: "show avatar of yourself or another user",
+  category: "Utility",
+  alias: ["av", "pfp"],
+  execute(message, args) {
+    let avatarFormat = { format: "png", dynamic: true, size: 2048 };
+    desiredMember = helper.FindUser(helper.JoinArgs(args), message);
+
+    if (!desiredMember) {
+      message.reply("couldn't find user!");
+      return;
+    }
+
+    if (!args.length) {
+      desiredMember = message.member;
+    }
+
+    const avatarEmbed = new Discord.MessageEmbed()
+      .setColor("#005eff")
+      .setAuthor(`${desiredMember.displayName}'s avatar`)
+      .setImage(desiredMember.user.avatarURL(avatarFormat))
+      .setFooter(`Requested by ${message.member.displayName}`, message.member.user.avatarURL(avatarFormat));
+
+    message.channel.send(avatarEmbed);
+  },
+};
