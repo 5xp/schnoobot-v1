8fa591c9840292da3146c0d0cc00bbfd1085cc5e
cflo <62446202+chrishops@users.noreply.github.com>
2021-07-15 12:58:55 -0500

    remove deploy from command handler

--- commands/command-handler.js
+++ commands/command-handler.js
@@ -6,7 +6,7 @@ const { getPrefix } = require("../utils/guildsettings");
 module.exports = client => {
   client.commands = [];
 
-  const readCommands = dir => {
+  const readCommands = async dir => {
     const files = fs.readdirSync(path.join(__dirname, dir));
     for (const file of files) {
       const stat = fs.lstatSync(path.join(__dirname, dir, file));
@@ -14,7 +14,7 @@ module.exports = client => {
         readCommands(path.join(dir, file));
       } else if (file !== handlerFile && file.endsWith(".js")) {
         const command = require(path.join(__dirname, dir, file));
-        let { name, description = "", usage = "", disabled = false, hidden = false, required_perms = [], execute } = command;
+        let { name, required_perms = [] } = command;
 
         if (typeof name === "string") command.name = [name];
         if (typeof required_perms === "string") command.required_perms = [required_perms];
@@ -30,20 +30,6 @@ module.exports = client => {
     const { member, content, guild } = message;
     const prefix = await getPrefix(guild?.id);
 
-    if (!client.application?.owner) await client.application?.fetch();
-
-    // temporary command to deploy commands to current guild
-    if (message.content.toLowerCase() === `${prefix}deploy` && message.author.id === client.application?.owner.id) {
-      const slashCommands = [];
-      for (const command of client.commands) {
-        let { name, description = "This description is empty!", options = null } = command;
-        const cmd = { name: name[0], description };
-        if (options) cmd.options = options;
-        slashCommands.push(cmd);
-      }
-      await message.guild?.commands.set(slashCommands);
-    }
-
     if (content.startsWith(prefix)) {
       for (const command of client.commands) {
         let { name, description = "", usage = "", disabled = false, hidden = false, required_perms = [], execute } = command;
@@ -52,7 +38,7 @@ module.exports = client => {
           if (content.toLowerCase().split(" ")[0] === `${prefix}${alias.toLowerCase()}`) {
             for (const permission of required_perms) {
               // TODO: list missing permissions
-              if (!member.hasPermission(permission)) {
+              if (!member.permissions.has(permission)) {
                 return message.reply(`missing permissions!`);
               }
             }
--- schnoobot.js
+++ schnoobot.js
@@ -5,10 +5,12 @@ const { Client } = require("discord.js");
 const mongo = require("./utils/mongo.js");
 const { HandleCoin } = require("./utils/coin");
 
-const client = new Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_EMOJIS", "GUILD_MESSAGE_REACTIONS"] });
+const client = new Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_EMOJIS", "GUILD_MESSAGE_REACTIONS"], partials: ["CHANNEL"] });
 
 client.once("ready", async () => {
   console.log(`Schnoobot is online!`.blue);
+  if (!client.application?.owner) await client.application?.fetch();
+  client.user.setActivity(`for ${process.env.PREFIX}`, { type: "WATCHING" });
 
   const commandHandler = require(`./commands/command-handler.js`);
 
