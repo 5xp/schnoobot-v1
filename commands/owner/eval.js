086068d62f14121af9cea0a84fb422c90db8a029
cflo <62446202+chrishops@users.noreply.github.com>
2021-07-15 23:29:52 -0500

    Eval slash command

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,13 +1,24 @@
-const helper = require("../../utils/helper.js");
-const { MessageAttachment } = require("discord.js");
+const { MessageAttachment, MessageButton } = require("discord.js");
 module.exports = {
   name: "eval",
-  async execute(message, args) {
-    if (message.author.id !== process.env.OWNERID) {
+  description: "execute code",
+  slash: true,
+  options: [{ name: "code", type: "STRING", description: "the code to execute", required: true }],
+  defaultPermission: false,
+  slashPermissions: [
+    {
+      id: "181104522892935168",
+      type: "USER",
+      permission: true,
+    },
+  ],
+  async execute(interaction, args, content) {
+    if (interaction.member.id !== interaction.client.application?.owner.id) {
       return;
     }
+    await interaction.defer?.({ ephemeral: true });
     try {
-      let code = helper.JoinArgs(args);
+      let code = content || interaction?.options.get?.("code")?.value;
       let evaled = await eval(code);
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
       let cleaned = clean(evaled);
@@ -15,12 +26,25 @@ module.exports = {
       if (cleaned.length > 2000) {
         const buffer = Buffer.from(cleaned, "utf-8");
         const attachment = new MessageAttachment(buffer, "response.xl");
-        message.reply({ files: [attachment], allowedMentions: { repliedUser: false } });
+
+        if (interaction.isCommand?.()) {
+          const msg = await interaction.editReply({
+            content: "⚠ **Response is too large - cannot respond ephemerally**",
+            components: [[new MessageButton().setLabel("Send anyway").setStyle("DANGER").setCustomId("send")]],
+          });
+
+          msg.awaitMessageComponent({ time: 15000 }).then(async i => {
+            await interaction.followUp({ files: [attachment] });
+            i.deferUpdate();
+          });
+        } else interaction.reply({ files: [attachment], allowedMentions: { repliedUser: false } });
       } else {
-        message.reply({ content: `\`\`\`xl\n${cleaned}\`\`\``, allowedMentions: { repliedUser: false } });
+        if (interaction.isCommand?.()) interaction.editReply({ content: `\`\`\`xl\n${cleaned}\`\`\`` });
+        else interaction.reply({ content: `\`\`\`xl\n${cleaned}\`\`\``, allowedMentions: { repliedUser: false } });
       }
     } catch (err) {
-      message.reply({ content: `\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``, allowedMentions: { repliedUser: false } });
+      if (interaction.isCommand?.()) interaction.followUp({ content: `\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``, allowedMentions: { repliedUser: false } });
+      else interaction.reply({ content: `\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``, allowedMentions: { repliedUser: false } });
     }
   },
 };
