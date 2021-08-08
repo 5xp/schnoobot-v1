f2aa2297e414f5ae96fdb01f1e891b7c4fd95999
chris <62446202+5xp@users.noreply.github.com>
2021-08-08 17:24:38 -0500

    coinflip slash command

--- commands/casino/flip.js
+++ commands/casino/flip.js
@@ -5,13 +5,39 @@ module.exports = {
   name: ["flip", "coinflip"],
   description: "flip a coin",
   usage: `${process.env.PREFIX}flip <heads/tails> <bet>`,
-  async execute(message, args) {
+  slash: true,
+  options: [
+    {
+      name: "face",
+      type: "STRING",
+      description: "heads or tails",
+      required: true,
+      choices: [
+        { name: "heads", value: "heads" },
+        { name: "tails", value: "tails" },
+      ],
+    },
+    {
+      name: "bet",
+      type: "STRING",
+      description: "your wager on this bet",
+      required: true,
+    },
+  ],
+  async execute(interaction, args) {
+    const isSlash = interaction.isCommand?.();
+    const user = isSlash ? interaction.user : interaction.author;
+
     let input, wager;
-    if (args[0] && args[1]) {
+
+    if (isSlash) {
+      input = interaction.options.getString("face");
+      wager = formatWager(interaction.options.getString("bet"));
+    } else if (args[0] && args[1]) {
       input = args[0].toLowerCase();
       wager = formatWager(args[1]);
     } else {
-      return message.reply(`⚠ **To play, use this command: \`${module.exports.usage}\`**`);
+      return interaction.reply(`⚠ **To play, use this command: \`${module.exports.usage}\`**`);
     }
 
     switch (input) {
@@ -24,27 +50,24 @@ module.exports = {
         input = 1;
         break;
       default:
-        return message.reply({ content: "⚠ **You must pick heads or tails.**", ephemeral: true });
+        return interaction.reply({ content: "⚠ **You must pick heads or tails.**" });
     }
 
-    const balance = await getBalance(message.author.id);
+    const balance = await getBalance(user.id);
     if (wager === "all") wager = balance;
 
     if (wager > balance) {
-      return message.reply({
+      return interaction.reply({
         content: `🚫 **Insufficient balance! Your balance is ${formatMoney(balance)}**.`,
         ephemeral: true,
       });
     } else if (wager < 0.01) {
-      return message.reply({ content: `🚫 **You must bet more than $0.00.**`, ephemeral: true });
+      return interaction.reply({ content: `🚫 **You must bet more than $0.00.**`, ephemeral: true });
     }
 
     const flipEmbed = new MessageEmbed()
       .setTitle("🪙 Coin Flip")
-      .setFooter(
-        message.member.displayName,
-        message.member.user.avatarURL({ format: "png", dynamic: true, size: 2048 })
-      )
+      .setFooter(user.username, user.avatarURL({ format: "png", dynamic: true, size: 2048 }))
       .setTimestamp();
 
     const flip = Math.round(Math.random());
@@ -55,19 +78,19 @@ module.exports = {
       flipEmbed.setColor("#2bff00");
       flipEmbed.addField("**Net Gain**", formatMoney(wager), true);
       flipEmbed.addField("**Balance**", formatMoney(balance + wager), true);
-      awardMoney(message.author.id, wager);
+      awardMoney(user.id, wager);
     } else {
       end = "**You lost!** ";
       flipEmbed.setColor("#ff0000");
       flipEmbed.addField("**Net Gain**", formatMoney(-wager), true);
       flipEmbed.addField("**Balance**", formatMoney(balance - wager), true);
-      awardMoney(message.author.id, -wager);
+      awardMoney(user.id, -wager);
     }
 
-    const str = flip === 0 ? end + `The coin landed on **heads**!` : end + `The coin landed on **tails**!`;
+    const outcome = flip === 0 ? end + `The coin landed on **heads**!` : end + `The coin landed on **tails**!`;
 
-    flipEmbed.setDescription(str);
+    flipEmbed.setDescription(outcome);
 
-    message.reply({ embeds: [flipEmbed], allowedMentions: { repliedUser: false } });
+    interaction.reply({ embeds: [flipEmbed], allowedMentions: { repliedUser: false } });
   },
 };
