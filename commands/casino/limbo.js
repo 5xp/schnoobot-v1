f1d9af2e4bfbfa177c50486e3ff8b322cc6fceaa
chris <62446202+5xp@users.noreply.github.com>
2021-08-21 19:29:54 -0500

    limbo play again and martingale button

--- commands/casino/limbo.js
+++ commands/casino/limbo.js
@@ -1,5 +1,4 @@
-const crypto = require("crypto");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 const numeral = require("numeral");
 const { awardMoney, getBalance, formatMoney, formatWager, toNumber } = require("@utils/economy");
 
@@ -13,24 +12,23 @@ module.exports = {
     { name: "bet", type: "STRING", description: "your wager on this bet", required: true },
   ],
   async execute(interaction, args) {
-    const isSlash = interaction.isCommand?.();
-    let input, wager, user;
+    const isSlash = interaction.isCommand?.() || interaction.isInteraction;
+    const user = isSlash ? interaction.user : interaction.author;
+    let target, wager;
 
-    if (isSlash) {
-      input = toNumber(interaction.options.getString("target"));
+    if (interaction.newGame) {
+      ({ target, wager } = interaction);
+    } else if (isSlash) {
+      target = toNumber(interaction.options.getString("target"));
       wager = formatWager(interaction.options.getString("bet"));
-      user = interaction.user;
+    } else if (args[0] && args[1]) {
+      target = toNumber(args[0]);
+      wager = formatWager(args[1]);
     } else {
-      user = interaction.author;
-      if (args[0] && args[1]) {
-        input = toNumber(args[0]);
-        wager = formatWager(args[1]);
-      } else {
-        return interaction.reply(`⚠ **To play, use this command: \`${module.exports.usage}\`**`);
-      }
+      return interaction.reply(`⚠ **To play, use this command: \`${module.exports.usage}\`**`);
     }
 
-    if (input <= 1) {
+    if (target <= 1) {
       return interaction.reply({ content: `⚠ **Your target payout must be greater than 1.00x.**`, ephemeral: true });
     }
 
@@ -46,19 +44,22 @@ module.exports = {
       return interaction.reply({ content: `🚫 **You must bet more than $0.00.**`, ephemeral: true });
     }
 
+    const profit = wager * (target - 1);
+
     const point = generatePoint();
+
+    const won = target <= point;
 
     const limboEmbed = new MessageEmbed()
       .setTitle("📈 Limbo")
       .setDescription(`Multiplier: **${point.toFixed(2)}x**`)
-      .addField("**Target Multipler**", input.toFixed(2) + "x", true)
+      .addField("**Target Multipler**", target.toFixed(2) + "x", true)
       .addField("**Bet**", formatMoney(wager), true)
-      .addField("**Win Chance**", numeral(1 / input).format("0.00%"), true)
+      .addField("**Win Chance**", numeral(1 / target).format("0.00%"), true)
       .setFooter(user.username, user.avatarURL({ format: "png", dynamic: true, size: 2048 }))
       .setTimestamp();
 
-    if (input <= point) {
-      const profit = wager * (input - 1);
+    if (won) {
       limboEmbed.setColor("#2bff00");
       limboEmbed.addField("**Net Gain**", formatMoney(profit), true);
       limboEmbed.addField("**Balance**", formatMoney(balance + profit), true);
@@ -69,15 +70,66 @@ module.exports = {
       limboEmbed.addField("**Balance**", formatMoney(balance - wager), true);
       awardMoney(user.id, -wager);
     }
-    interaction.reply({ embeds: [limboEmbed], allowedMentions: { repliedUser: false } });
+
+    const mult = target / (target - 1);
+
+    const playAgainButton = new MessageButton({ style: "PRIMARY", label: "Play again", customId: "playAgain" });
+    const martingaleButton = new MessageButton({
+      style: "DANGER",
+      label: `Play again (${mult.toFixed(2)}x bet)`,
+      customId: "martingale",
+    });
+
+    const buttonRow = () => {
+      const row = new MessageActionRow().addComponents([playAgainButton]);
+      if (!won) row.addComponents([martingaleButton]);
+      return row;
+    };
+
+    const reply = await interaction.reply({
+      embeds: [limboEmbed],
+      components: [buttonRow()],
+      allowedMentions: { repliedUser: false },
+      fetchReply: true,
+    });
+
+    const i = await reply.awaitMessageComponent({ filter: i => i.user.id === user.id, time: 10000 }).catch(() => {
+      // ignore error
+    });
+
+    playAgainButton.setDisabled();
+    martingaleButton.setDisabled();
+
+    if (i) {
+      i.originalBet = interaction.originalBet ?? wager;
+      i.newGame = true;
+      i.wager = won ? i.originalBet : wager;
+      i.target = target;
+
+      if (i.customId === "martingale") {
+        i.wager *= mult;
+      }
+
+      if (isSlash) {
+        i.isInteraction = true;
+        i.options = interaction.options;
+        module.exports.execute(i);
+      } else {
+        i.author = interaction.author;
+        module.exports.execute(i);
+      }
+    } else {
+      playAgainButton.setStyle("SECONDARY");
+      martingaleButton.setStyle("SECONDARY");
+    }
+
+    reply.edit({ components: [buttonRow()] });
   },
 };
 
 function generatePoint() {
-  const hex = crypto.randomBytes(32).toString("hex");
+  const point = 1 / Math.random();
 
-  const h = parseInt(hex.slice(0, 52 / 4), 16);
-  const e = Math.pow(2, 52);
-
-  return Math.floor((100 * e) / (e - h)) / 100;
+  // truncate to 2 decimals
+  return Math.floor(point * 100) / 100;
 }
